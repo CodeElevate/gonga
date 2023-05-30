fb9ea5e4794fe83f70ea8d4a23efd7ed571f6633
kkumar-gcc <krishkumar9352@gmail.com>
2023-05-30 09:36:59 +0530

    Add imaginary package for fake image generation

--- database/Factories/MediaFactory.go
+++ database/Factories/MediaFactory.go
@@ -2,6 +2,7 @@ package factory
 
 import (
 	"gonga/app/Models"
+	imaginary "gonga/packages/Imaginary"
 
 	"github.com/jaswdr/faker"
 )
@@ -9,10 +10,11 @@ import (
 // MediaFactory generates a fake media instance
 func MediaFactory() Models.Media {
 	faker := faker.New()
+	image := imaginary.GenerateImage(800, 600)
 
 	media := Models.Media{
-		// URL:       faker.LoremFlickr().,
-		Type:      faker.MimeType().MimeType(),
+		URL:       image.URL,
+		Type:      image.Type,
 		OwnerID:   0,                                                                 // Set the appropriate owner ID here
 		OwnerType: faker.RandomStringElement([]string{"posts", "comments", "users"}), // Set the appropriate owner type here
 	}
